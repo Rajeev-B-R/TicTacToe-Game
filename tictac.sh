899a1c469c3e2d275d809ec842f8446222ad560d
Rajeev <rajeevsuchiha@gmail.com>
2020-08-24 11:27:22 +0530

    Added: Code to determine win move or change turn

--- tictac.sh
+++ tictac.sh
@@ -3,6 +3,7 @@ echo "***************TicTacToe Program***************"
 
 declare -A tictac
 
+count=1
 rows=3
 columns=3
 
@@ -38,10 +39,10 @@ function toss()
 	if [[ $tossCheck -eq 0 ]]
 	then
 		echo "Player1 wins toss!"
-    		player1=$(letter)
+    	player1=$(letter)
 	else
 		echo "Player2 wins toss!"
-    		player2=$(letter)    
+    	player2=$(letter)    
 	fi
 }
 
@@ -60,4 +61,95 @@ function boardSee()
 	done
 }
 
+function chngePlayer()
+{
+	if [[ $1 == "X" ]]
+	then
+		currPlayer="O"
+	else
+		currPlayer="X"
+	fi
+}
+
+function winCheck()
+{
+	match3=0
+	match4=0
+	for (( i=1; i<=3; i++ ))
+	do
+		match1=0
+		match2=0
+
+		for (( j=1; j<=3; j++ ))
+		do
+			if [[ ${board[$i,$j]} == $1 ]]
+			then
+				match1=$((match1+1))
+			fi
+		done
+
+		for (( k=1; k<=3; k++ ))
+		do
+			if [[ ${board[$k,$i]} == $1 ]]
+			then
+				match2=$((match2+1))
+			fi
+		done
+
+		if [[ ${board[$i,$i]} == $1 ]]
+		then
+			match3=$((match3+1))
+		fi
+
+		for (( y=1; y<=3; y++ ))
+		do
+			add=$((i+y))
+			if [[ $add == 4 && ${board[$i,$y]} == $1 ]]
+			then
+				match4=$((match4+1))
+			fi
+		done
+	
+  if [[ $match1 == 3 || $match2 == 3 || $match3 == 3 || $match4 == 3 ]]
+	then
+		echo "$1 wins!"
+		exit
+	elif [[ $count == 9 ]]
+	then
+		echo "A Tie!"
+	fi
+	done
+	count=$((count+1))
+}
+
+function game()
+{
+	row1=$1
+	column1=$2
+	if [[ ${tictac[$row1,$column1]} == "-" ]]
+	then
+		tictac[$row1,$column1]=$currPlayer
+		boardSee
+		winCheck $currPlayer
+		chngePlayer $currPlayer
+	else
+		echo "No such place"
+	fi
+}
+
+reset
+toss
 boardSee
+while [[ $count -le 9 ]]
+do
+	if [[ $currPlayer == "X" ]]
+	then
+		read -p "Enter row index: " rowPos
+		read -p "Enter column index: " columnPos
+		game $rowPos $columnPos
+	else
+		read -p "Enter row index: " rowPos
+		read -p "Enter column index: " columnPos
+		game $rowPos $columnPos
+	fi
+done
