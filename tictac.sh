5733e28002fada299e2c053824ee45a2d8c493fc
Rajeev <rajeevsuchiha@gmail.com>
2020-08-24 09:03:26 +0530

    Added: Code added to start afresh with resetting board

--- tictac.sh
+++ tictac.sh
@@ -1,3 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash -x  
 echo "***************TicTacToe Program***************"
 
+declare -A tictac
+
+rows=3
+columns=3
+
+function reset()
+{
+	for (( i=1; i<=$rows; i++ ))
+	do
+		for (( j=1; j<=$columns; j++ ))
+		do
+			tictac[$i,$j]="*"
+		done
+	done
+}
+
+reset
